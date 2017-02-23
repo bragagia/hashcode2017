fcf835acddce65f3aaf8a0b5ccd514da82dcab0f
Maxime Parriaux <maxime.parriaux@epitech.eu>
2017-02-23 21:09:14 +0100

    lol

--- main.py
+++ main.py
@@ -1,26 +1,28 @@
 import sys
 from endpoint import *
 
-V = -1
-R = -1
-X = -1
-E = -1
-C = -1
+config = {
+    'V' : -1,
+    'R' : -1,
+    'X' : -1,
+    'E' : -1,
+    'C' : -1
+}
 
-def getInfo(File):
+def getInfo(File, config):
     for i in File:
         tmp = i
         tab = tmp.split(" ");
-        V = tab[0]
-        E = tab[1]
-        R = tab[2]
-        C = tab[3]
-        X = tab[4]
+        config['V'] = int(tab[0])
+        config['E'] = int(tab[1])
+        config['R'] = int(tab[2])
+        config['C'] = int(tab[3])
+        config['X'] = int(tab[4])
         break
 
 if len(sys.argv) <= 1:
     exit(42)
 else:
     File = open(sys.argv[1], 'r');
-    getInfo(File)
-    getEndPoint(File)
+    getInfo(File, config)
+    getEndPoint(File, config)
